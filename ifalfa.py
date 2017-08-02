4351f8e50aa621a9a208e653132c9093f7aac2f0
Rajneesh Kumar Sharma <coolrajnish.sharma@gmail.com>
2017-08-02 11:39:25 +0530

    if alpha

---
+++ ifalfa.py
@@ -0,0 +1,7 @@
+print 'Welcome to the Translater'
+original=raw_input("write a word:")
+if len(original)>0 and original.isalpha():
+  print original.isalpha()
+else:
+  print "word charcter is less the 0"
+
